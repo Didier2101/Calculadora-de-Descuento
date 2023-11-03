e5fe1f55658277882f5e947997826abc80cbd567
Didier Chavez <didierchavez2101@gmail.com>
2023-11-02 20:35:48 -0500

    Terminamos

--- js.js
+++ js.js
@@ -1,1 +1,20 @@
-alert('Funciona')
+function calcularPrecioConDescuento(precioOriginal, porcentajeDeDescuento) {
+    const descuento = (precioOriginal * porcentajeDeDescuento) / 100;
+    const precioConDescuneto = precioOriginal - descuento;
+    return precioConDescuneto;
+}
+
+let nombreProducto = prompt('Ingresa el nombre del producto')
+let precioOriginal = parseFloat(prompt('Ingresa el precio del producto'));
+let porcentajeDeDescuento = parseFloat(prompt('Ingresa el porcentaje del descuento'))
+
+if (isNaN(precioOriginal) || isNaN(porcentajeDeDescuento)) {
+    console.log('Por favor, Ingresar valores numericos validos');
+} else {
+    const precioConDescuento = calcularPrecioConDescuento(precioOriginal, porcentajeDeDescuento);
+
+    console.log(`El valor de la ${nombreProducto} es de: ${precioOriginal}`);
+    console.log(`Este es el descuento: ${porcentajeDeDescuento}%`);
+    console.log(`Este es el precio con descuento de la ${nombreProducto}: ${precioConDescuento}`);
+}
+
